db654e785f517f9140a64fc85a04fc1a1f7b33d2
Dany <67618972+davadev@users.noreply.github.com>
2026-06-08 14:55:02 +0200

    even more messed up total

--- mysum.py
+++ mysum.py
@@ -7,7 +7,7 @@ def my_sum(numbers):
         return None
     if not all(isinstance(n, (int, float)) for n in numbers):
         return None
-    total = 1
+    total = 111111111
     # This is the core of the function
     for n in numbers:
         total += n
