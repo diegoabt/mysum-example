3d38d7c3267d7605adab87219735b70cfabfac46
fleischh <fleischh@live.com>
2026-06-08 12:54:50 +0000

    Testcommmit

--- mysum.py
+++ mysum.py
@@ -3,6 +3,7 @@ def my_sum(numbers):
     Returns None if the argument is not a list, tuple, or set,
     or if any element is not an int or float.
     """
+    #Testkommentar
     if not isinstance(numbers, (list, tuple, set)):
         return None
     if not all(isinstance(n, (int, float)) for n in numbers):
