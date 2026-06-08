44341ae61e4ddb18da760f8d89850cb79c57e3f6
ChristosBerger <arvakur@live.at>
2026-06-08 12:53:05 +0000

    most useful comment ever

--- mysum.py
+++ mysum.py
@@ -3,6 +3,7 @@ def my_sum(numbers):
     Returns None if the argument is not a list, tuple, or set,
     or if any element is not an int or float.
     """
+    #yippi ai yey!
     if not isinstance(numbers, (list, tuple, set)):
         return None
     if not all(isinstance(n, (int, float)) for n in numbers):
@@ -12,3 +13,4 @@ def my_sum(numbers):
     for n in numbers:
         total += n
     return total
+# uh push it
